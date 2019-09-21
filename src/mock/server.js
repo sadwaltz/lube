72275e15d9686531ac3cf64ee94a827fb21b61d8
sadwaltz <zhaiwhappy@126.com>
2019-09-21 10:46:58 +0800

    wrong commit

---
+++ src/mock/server.js
@@ -0,0 +1,54 @@
+var express = require('express');
+var app = express();
+app.get('/', function (req, res) {  
+    res.send('Hello world!');
+});
+
+app.get('/api1', function (req, res) {  
+    let temp = Date.now();
+    let data = {
+        data : [
+            {
+                sell : {
+                    x : temp,
+                    y : Math.random()
+                },
+                buy : {
+                    x : temp,
+                    y : Math.random()
+                }
+            },
+            
+        ]
+        
+        
+        
+    };
+    //res.send('Hello world!'+req.query.future);
+    res.json(data);
+});
+app.get('/api', function (req, res) {  
+    let temp = Date.now();
+    let data = {
+        data : [
+            {
+                sell : {
+                    x : temp,
+                    y : Math.random()
+                },
+                buy : {
+                    x : temp,
+                    y : Math.random()
+                }
+            },
+            
+        ],
+        success : true
+        
+        
+        
+    };
+    //res.send('Hello world!'+req.query.future);
+    res.json(data);
+});
+app.listen(5000); 
